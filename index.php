f1be0ed1004ffb094cc1cc64edb40f6468083212
Lucas Andrade <luksandrades18@gmail.com>
2018-10-18 14:19:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<html lang="pt-br">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset="utf-8">
+        <style>
+            @import url('https://fonts.googleapis.com/css?family=Rubik');
+
+            h2 {
+                text-align: center;
+                font-family: 'Rubik', sans-serif;
+                font-size: 35px
+            }
+
+            h1 {
+                text-align: center;
+                font-family: 'Rubik', sans-serif;
+                font-size: 55px
+            }
+
+            .justify-content-center {
+                -ms-flex-pack: center !important;
+                justify-content: center !important
+            }
+
+            .img-fluid {
+                max-width: 100%;
+                height: auto
+            }
+
+            .text-center {
+                text-align: center !important
+            }
+        </style>
+    </head>
+    <body>
+        <div class="corpo justify-content-center">
+            <h2>Como Formamos?</h2>
+            <div class="text-center">
+                <img class="img-fluid" src="natora.gif">
+            </div>
+            <h1>NA TORA!!!!!</h1>
+        </div>
+    </body>
+</html>
